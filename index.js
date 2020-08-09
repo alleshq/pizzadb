8082ff51e497eae2af9a0c446ebd843770cdf341
Archie Baer <archie@abaer.dev>
2020-08-09 21:45:39 +0100

    Simple Express API

---
+++ index.js
@@ -0,0 +1,12 @@
+// Express
+const express = require("express");
+const app = express();
+app.use(require("body-parser").json());
+app.use((_err, _req, res, _next) => res.status(500).json({ err: "internalError" }));
+app.listen(8080, () => console.log("Express is listening..."));
+
+// Base route
+app.get("/", (_req, res) => res.send("PizzaDB"));
+
+// 404
+app.use((_req, res) => res.status(404).json({ err: "notFound" }));
