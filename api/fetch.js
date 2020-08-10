fe378c366219d19610270ae13ad43ab7212e0be5
Archie Baer <archie@abaer.dev>
2020-08-10 02:56:35 +0100

    Adjusted to use badAuthorization error securely

--- api/fetch.js
+++ api/fetch.js
@@ -1,26 +1,24 @@
 const db = require('../db')
 
 module.exports = async (req, res) => {
+  // Get base
+  const base = await db.Base.findOne({
+    where: {
+      id: req.params.base
+    }
+  })
+  if (!base) return res.status(404).json({ err: 'notFound' })
+  if (!base.public && base.secret !== req.headers.authorization) return res.status(401).json({ err: 'badAuthorization' })
+
   // Get key/value record
   const kv = await db.Data.findOne({
     where: {
-      base: req.params.base,
+      base: base.id,
       key: req.params.key,
       value: req.params.value
     }
   })
   if (!kv) return res.status(404).json({ err: 'notFound' })
-
-  // Get base
-  const base = await db.Base.findOne({
-    where: {
-      id: kv.base
-    }
-  })
-  if (
-    !base ||
-      (!base.public && base.secret !== req.headers.authorization)
-  ) return res.status(404).json({ err: 'notFound' })
 
   // Get all record data
   const recordData = await db.Data.findAll({
